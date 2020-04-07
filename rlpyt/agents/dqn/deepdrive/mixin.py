59d3bad5b1f06c50fcd17a0094b24b08e6481f55
Isaac Kargar <kargarisaac@yahoo.com>
2020-04-07 10:39:37 +0300

    dellete some files

--- rlpyt/agents/dqn/deepdrive/mixin.py
+++
@@ -1,6 +0,0 @@
-
-class DeepDriveMixin:
-
-    def make_env_to_model_kwargs(self, env_spaces):
-        return dict(observation_shape=env_spaces.observation.shape,
-                    output_size=env_spaces.action.n)
